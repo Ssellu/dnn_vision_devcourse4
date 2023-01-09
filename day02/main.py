ee020099ea93cfc1241da0da034b22ddbf8d24fb
Sera lee <ssellu.lee@gmail.com>
2023-01-10 06:23:45 +0900

    Day02_02 Pooling and Fully Connected NN

--- day02/main.py
+++ day02/main.py
@@ -11,7 +11,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 from function.convolution import MyConv
-
+from function.pooling import Pool
+from function.fc import FC
 
 def convolution():
     print("Convolution")
@@ -62,12 +63,12 @@ def convolution():
     print(L2)
 
     # pytorch Conv
-    torch_conv = nn.Conv2d(in_c, 
-                           out_c, 
+    torch_conv = nn.Conv2d(in_c,
+                           out_c,
                            kernel_size=k_h,
-                           stride=1, 
-                           padding=0, 
-                           bias=False, 
+                           stride=1,
+                           padding=0,
+                           bias=False,
                            dtype=torch.float32)
     torch_conv.weight = nn.Parameter(torch.tensor(W, dtype=torch.float32))
     L3 = torch_conv(torch.tensor(X, requires_grad=False, dtype=torch.float32))
@@ -75,5 +76,65 @@ def convolution():
     print(L3)
 
 
-if __name__ == '__main__':
-    convolution()
+def forward_net():
+    """_summary_
+    'Conv - Pooling - FC' model inference code 
+    """
+    #define
+    batch = 1
+    in_c = 3
+    in_w = 6
+    in_h = 6
+    k_h = 3
+    k_w = 3
+    out_c = 1
+    
+    X = np.arange(batch*in_c*in_w*in_h, dtype=np.float32).reshape([batch,in_c,in_w,in_h])
+    W1 = np.array(np.random.standard_normal([out_c,in_c,k_h,k_w]), dtype=np.float32)
+    
+    Convolution = MyConv(batch = batch,
+                        in_c = in_c,
+                        out_c = out_c,
+                        in_h = in_h,
+                        in_w = in_w,
+                        k_h = k_h,
+                        k_w = k_w,
+                        dilation = 1,
+                        stride = 1,
+                        pad = 0)
+    
+    L1 = Convolution.gemm(X,W1)
+    
+    print("L1 shape : ", L1.shape)
+    print(L1)
+    
+    Pooling = Pool(batch=batch,
+                   in_c = 1,
+                   out_c = 1,
+                   in_h = 4,
+                   in_w = 4,
+                   kernel=2,
+                   dilation=1,
+                   stride=2,
+                   pad = 0)
+    
+    L1_MAX = Pooling.pool(L1)
+    print("L1_MAX shape : ", L1_MAX.shape)
+    print(L1_MAX)
+    
+    #fully connected layer
+    W2 = np.array(np.random.standard_normal([1, L1_MAX.shape[1] * L1_MAX.shape[2] * L1_MAX.shape[3]]), dtype=np.float32)
+    Fc = FC(batch = L1_MAX.shape[0],
+            in_c = L1_MAX.shape[1],
+            out_c = 1,
+            in_h = L1_MAX.shape[2],
+            in_w = L1_MAX.shape[3])
+
+    L2 = Fc.fc(L1_MAX, W2)
+    
+    print("L2 shape : ", L2.shape)
+    print(L2)
+
+if __name__  == '__main__':
+    # convolution()
+    forward_net()
